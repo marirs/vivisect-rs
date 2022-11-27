ffbc771fc328b1ec7787c679f7bd9ba0f75153f6
marirs <marirs@gmail.com>
2022-11-27 18:41:37 +0530

    example

---
+++ examples/main.rs
@@ -0,0 +1,18 @@
+use std::env::current_dir;
+use std::rc::Rc;
+use log::Level;
+use simple_logger::init_with_level;
+use vivisect::analysis::{EntryPointsAnalyzer, RelocationsAnalyzer};
+use vivisect::workspace::VivWorkspace;
+
+pub fn main() {
+    init_with_level(Level::Trace).unwrap();
+    println!("{:?}", current_dir());
+    let sample_path = "data/test-decode-to-stack.exe";
+    let mut workspace = VivWorkspace::new("", false);
+    workspace.load_from_file(sample_path, None, None);
+    workspace.add_analyzer(Rc::new(RelocationsAnalyzer::new()));
+    workspace.add_analyzer(Rc::new(EntryPointsAnalyzer::new()));
+    workspace.analyze(sample_path);
+    // println!("{:?}", workspace);
+}
